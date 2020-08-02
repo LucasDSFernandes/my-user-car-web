149f56cc8d1b9e68e635f27bbcf74f62f23d42a7
Lucas <lucas.dos.fernandes@accenture.com>
2020-08-02 20:43:17 -0300

    fix(bug) retirnado data de criacao e data de ultimo acesso.

--- src/app/pages/off/register/register.component.ts
+++ src/app/pages/off/register/register.component.ts
@@ -110,8 +110,6 @@ export class RegisterComponent implements OnInit {
       email: this.registerForm.get('email').value,
       phone: this.registerForm.get('phone').value,
       birthday: this.registerForm.get('birthday').value,
-      lastLogin: this.registerForm.get('lastLogin').value,
-      createdAt: this.registerForm.get('createdAt').value,
       cars: this.registerForm.get('cars').value,
       login: this.registerForm.get('login').value,
       password: this.registerForm.get('password').value
